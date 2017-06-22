672d81a2fcd384a946d75f78b264c67eb22e026b
VaneGarciaYara <vanegarciay@gmail.com>
2017-06-22 10:33:10 -0400

    intento por haceer el contador de km

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,8 @@ function initMap() {
     var directionsService = new google.maps.DirectionsService;
     var directionsDisplay = new google.maps.DirectionsRenderer;
 
-    document.getElementById("ruta").addEventListener("click", function(){dibujarRuta(directionsService, directionsDisplay)});
+    document.getElementById("ruta").addEventListener("click", function(){
+        dibujarRuta(directionsService, directionsDisplay)});
 
     directionsDisplay.setMap(map);
 
@@ -143,5 +144,80 @@ function initMap() {
     function funcionErrorRuta() {
         alert("No ingresaste un origen y un destino validos");
     }
+/*
+    NO ME FUNCIONO EL CODIGO PARA CONTAR LOS KM
+    
+    function contarKm(origen,destino){
+      var bounds = new google.maps.LatLngBounds;
+      var markersArray = [];
 
+      var origen = document.getElementById("origen").value;
+      var destino = document.getElementById('destino').value;
+
+
+      var destinationIcon = crearMarcador();
+      var originIcon = crearMarcador();
+      var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 55.53, lng: 9.4},
+        zoom: 10
+      });
+      var geocoder = new google.maps.Geocoder;
+
+      var service = new google.maps.DistanceMatrixService;
+      service.getDistanceMatrix({
+        origins: [origen],
+        destinations: [destino],
+        travelMode: 'DRIVING',
+        unitSystem: google.maps.UnitSystem.METRIC,
+        avoidHighways: false,
+        avoidTolls: false
+      }, function(response, status) {
+        if (status !== 'OK') {
+          alert('Error was: ' + status);
+        } else {
+      var originList = response.originAddresses;
+      var destinationList = response.destinationAddresses;
+      var outputDiv = document.getElementById('ruta');
+      outputDiv.innerHTML = '';
+      deleteMarkers(markersArray);
+
+      var showGeocodedAddressOnMap = function(asDestination) {
+        var icon = asDestination ? destinationIcon : originIcon;
+        return function(results, status) {
+          if (status === 'OK') {
+            map.fitBounds(bounds.extend(results[0].geometry.location));
+            markersArray.push(new google.maps.Marker({
+              map: map,
+              position: results[0].geometry.location,
+              icon: icon
+            }));
+          } else {
+            alert('Geocode was not successful due to: ' + status);
+          }
+        };
+      };
+
+      for (var i = 0; i < originList.length; i++) {
+        var results = response.rows[i].elements;
+        geocoder.geocode({'address': originList[i]},
+            showGeocodedAddressOnMap(false));
+        for (var j = 0; j < results.length; j++) {
+          geocoder.geocode({'address': destinationList[j]},
+              showGeocodedAddressOnMap(true));
+          outputDiv.innerHTML += originList[i] + ' to ' + destinationList[j] +
+              ': ' + results[j].distance.text + ' in ' +
+              results[j].duration.text + '<br>';
+        }
+      }
+    }
+  });
 }
+function deleteMarkers(markersArray) {
+      for (var i = 0; i < markersArray.length; i++) {
+        markersArray[i].setMap(null);
+      }
+      markersArray = [];
+    }
+    */
+}
+
