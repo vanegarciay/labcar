9d018e40e3faf62a2a93d536b044ff39a12a1103
VaneGarciaYara <vanegarciay@gmail.com>
2017-06-22 11:32:48 -0400

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -134,6 +134,7 @@ function initMap() {
             function(response, status) {
                 if (status === "OK") {
                     directionsDisplay.setDirections(response);
+                    contarKm(origin,destination);
                 } else {
                     funcionErrorRuta();
                 }
@@ -145,81 +146,32 @@ function initMap() {
         alert("No ingresaste un origen y un destino validos");
     }
 
-    //NO ME FUNCIONO EL CODIGO PARA CONTAR LOS KM, CUANDO LO PONGO EN LA FUNCION Q DIBUJA LA RUTA, ME BORRA LA RUTA POR PONERME LOS KM
-/*
-    function contarKm(origen,destino){
-      var bounds = new google.maps.LatLngBounds;
-      var markersArray = [];
+function contarKm(origen,destino) {
+        var geocoder = new google.maps.Geocoder;
+        var service = new google.maps.DistanceMatrixService;
 
-      var origen = document.getElementById("origen").value;
-      var destino = document.getElementById('destino').value;
-
-
-      var destinationIcon = 'https://chart.googleapis.com/chart?' +
-            'chst=d_map_pin_letter&chld=D|FF0000|000000';
-        var originIcon = 'https://chart.googleapis.com/chart?' +
-            'chst=d_map_pin_letter&chld=O|FFFF00|000000';
-      var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 55.53, lng: 9.4},
-        zoom: 10
-      });
-      var geocoder = new google.maps.Geocoder;
-
-      var service = new google.maps.DistanceMatrixService;
-      service.getDistanceMatrix({
-        origins: [origen],
-        destinations: [destino],
-        travelMode: 'DRIVING',
-        unitSystem: google.maps.UnitSystem.METRIC,
-        avoidHighways: false,
-        avoidTolls: false
-      }, function(response, status) {
-        if (status !== 'OK') {
-          alert('Error was: ' + status);
-        } else {
-      var originList = response.originAddresses;
-      var destinationList = response.destinationAddresses;
-      var outputDiv = document.getElementById('ruta');
-      outputDiv.innerHTML = '';
-      deleteMarkers(markersArray);
-
-      var showGeocodedAddressOnMap = function(asDestination) {
-        var icon = asDestination ? destinationIcon : originIcon;
-        return function(results, status) {
-          if (status === 'OK') {
-            map.fitBounds(bounds.extend(results[0].geometry.location));
-            markersArray.push(new google.maps.Marker({
-              map: map,
-              position: results[0].geometry.location,
-              icon: icon
-            }));
-          } else {
-            alert('Geocode was not successful due to: ' + status);
-          }
-        };
-      };
-
-      for (var i = 0; i < originList.length; i++) {
-        var results = response.rows[i].elements;
-        geocoder.geocode({'address': originList[i]},
-            showGeocodedAddressOnMap(false));
-        for (var j = 0; j < results.length; j++) {
-          geocoder.geocode({'address': destinationList[j]},
-              showGeocodedAddressOnMap(true));
-          outputDiv.innerHTML += originList[i] + ' to ' + destinationList[j] +
-              ': ' + results[j].distance.text + ' in ' +
-              results[j].duration.text + '<br>';
-        }
-      }
+        service.getDistanceMatrix({
+            origins: [origen],
+            destinations: [destino],
+            travelMode: 'DRIVING',
+            unitSystem: google.maps.UnitSystem.METRIC,
+            avoidHighways: false,
+            avoidTolls: false
+        }, function(response, status) {
+            if (status !== 'OK') {
+                alert('Error was: ' + status);
+            } else {
+                var results = response.rows[0].elements;
+                console.log(results[0].distance.text + ' (' + results[0].duration.text + ')'); // Distancia y tiempo
+            }
+        });
     }
-  });
-}
 function deleteMarkers(markersArray) {
       for (var i = 0; i < markersArray.length; i++) {
         markersArray[i].setMap(null);
       }
       markersArray = [];
     }
-*/
+
 }
 
